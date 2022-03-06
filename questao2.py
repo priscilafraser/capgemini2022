6a74429edb2f4137394541c93d33274f2545f9c5
Priscila Miranda (fraser) <priscila.fraser@gmail.com>
2022-03-06 15:41:09 -0400

    questão2

--- questao2.py
+++ questao2.py
@@ -1,13 +1,25 @@
-l = [1, 5, 3, 4, 2]
-ls = [1, 5, 3, 4, 2]
+el = int(input('Digite a quantidade de elementos: '))
 
+vet = list()
+for i in range(1, el + 1):
+    while True:
+        try:
+            v = int(input(f'Digite o {i}º valor: '))
+            break
+        except ValueError:
+            print('Valor INVÁLIDO! Digite apenas valores inteiros!')
+    vet.append(v)
+    
+print(f'A lista de números digitados foi:\n{vet}')
+
+ls = vet
 pares = []
 cont =0
 resultado= []
-diferenca = int(input())
+diferenca = int(input("Digite o valor da diferença desejada: "))
 
 print("Os pares encontrados com a diferença especificada foram:")
-for i in l:
+for i in vet:
   for j in ls:
     menos = abs(i - j)
     pares.append(menos)
@@ -15,7 +27,6 @@ for i in l:
       print(f'|{i}-{j}|')
       resultado.append([i,j])
       cont += 1
-
 
 if cont == 0:
   print('Diferença não encontrada')
